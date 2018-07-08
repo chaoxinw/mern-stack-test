89bfff8973c9fa1a20cd8d6f2b1fcbf92462a945
Alan Wu <c.wu43@student.unimelb.edu.au>
2018-07-08 16:51:59 +1000

    change port

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ if (process.env.NODE_ENV === 'production') {
 	})
 }
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+
+app.listen(process.env.PORT);
 
